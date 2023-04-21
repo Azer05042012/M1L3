528899cdee8fd4e4bd207498c95945d2a3395c02
Azer05042012 <129597661+Azer05042012@users.noreply.github.com>
2023-04-21 20:07:08 +0400

    Add files via upload

---
+++ bot_logic.py
@@ -0,0 +1,30 @@
+import random
+
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
+    
+def guess_number():
+    number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    return random.choice(number) 
+
+
---
+++ settings.py
@@ -0,0 +1,4 @@
+settings = {
+    "prefix": ">",
+    "TOKEN": "MTA5NzkzNzA4Njk1MjI2Mzg0Mg.G2lKrG.ay64RVeLxmMKpiITSVzu3nmV9IqLzr8pMBsyFU"
+}
